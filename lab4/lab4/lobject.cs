b4a740062129b183adeb572904436fcc2c5e7277
RomanKudyba <48390125+RomanKudyba@users.noreply.github.com>
2020-03-30 02:38:25 +0300

    Add files via upload

---
+++ lab4/lab4/lobject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace lab4
+{
+    public class lobject
+    {
+        public int city;
+        public int cost;
+        public int[,] matrix;
+        public int[] remainingcity;
+        public int city_left_to_expand;
+        public Stack<int> st;
+        public lobject(int number)
+        {
+            matrix = new int[number, number];
+            st = new Stack<int>();
+        }
+    }
+}
